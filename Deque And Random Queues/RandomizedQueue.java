04a20a5ed07fc2107ac956327590e3474b7ebde5
Navd15 <gill.222navdeep@gmail.com>
2018-09-10 23:47:33 +0530

    Random Queue

---
+++ Deque And Random Queues/RandomizedQueue.java
@@ -0,0 +1,20 @@
+import java.util.Iterator;
+import edu.princeton.cs.algs4.*;
+
+public class RandomizedQueue<Item> implements Iterable<Item>{
+
+    public RandomizedQueue() {
+    }
+
+    public boolean isEmpty(){}
+    public int size(){}
+    public void enqueue(Item item){}
+    public Item dequeue(){}
+    public Item sample(){}
+
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
